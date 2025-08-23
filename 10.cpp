11505855c8d3c50fcc50385dee88f1366d42b1c5
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-23 21:49:31 +0600

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+float b;
+char c;
+double d;
+cout<<sizeof(a)<<"bytes\n";
+
+cout<<sizeof(b)<<"bytes\n";
+cout<<sizeof(c)<<"bytes\n";
+cout<<sizeof(d)<<"bytes\n";
+
+
+
+
+
+
+
+
+
+return 0;}
