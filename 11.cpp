a93fab995a963c877914049a38d139691975c7b4
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-23 22:42:40 +0600

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<complex>
+using namespace std;
+int main(){
+complex<float> a=2.0f+sqrt(complex<float>(-2.0f));
+complex< float> b=2.0f-sqrt(complex<float>(-4.0f));
+complex<float>c;
+c=a+b;
+cout<<c;
+
+
+
+
+
+
+
+
+return 0;}
