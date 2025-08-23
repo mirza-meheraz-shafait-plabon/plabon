dc924ed2f3c2fce7750568488b57519ddc99cfd7
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-24 01:32:56 +0600

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+float p=100,r=.22,t=3,n=4;
+float w=n*t;
+float a=p*pow(1+r*t,w) ;
+cout<<"total amount is "<<a;
+
+
+///interest is harom. this is jus to practice coding
+
+
+
+return 0;}
