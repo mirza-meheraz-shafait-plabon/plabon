97c6a0b94740bbf19e1dc86ac801783e17d4ade7
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-24 02:56:45 +0600

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+char a;
+cin>>a;
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+cout<<"vowel";}
+else{
+cout<<"consonant";}
+
+return 0;}
