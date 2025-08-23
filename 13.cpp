29d326640ddecd8e9c49b690dcb85f88d7c64ba3
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-24 00:46:13 +0600

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+float p=100,r=10,t=2;
+float i=p*r*t;
+cout<<"for100taka&with the interest rate of10 percent in2years, the interest is"<<i;
+
+////interest is harom. it is only for coding practice
+
+
+
+
+
+
+return 0;}
