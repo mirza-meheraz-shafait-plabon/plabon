daf0bece95b4bf35af0c66d841f5c896962448d9
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-24 19:10:45 +0600

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,15 @@
+#include<iostrean>
+using namespace std;
+int main(){
+int a=2,b=3,c=4;
+if(a>b&&b>c){
+cout<<"a is largest";}
+else if(b>>a&&a>>c){
+cout<<"b is largest";}
+else {cout<<"c is largest";}
+
+
+
+
+
+return 0;}
