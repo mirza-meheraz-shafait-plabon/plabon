11e815b5317f9eeaa00037721305de3d9b7a5d45
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-24 22:01:45 +0600

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int  main(){
+int num;
+cout<<"enter a number to generate it's multiplication table";
+cin>>num;
+for(int i=1;i<=10;i++){
+cout<<num*i<<" ";}
+
+
+
+
+
+return 0;}
