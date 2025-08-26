2ae081fcf8013b507663d7d5f5f500e309ad4e52
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-27 01:44:52 +0600

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int a=25,b,digit1,digit2;
+b=sqrt(a);
+digit1=a/10;
+digit2=a%10;
+if(digit1+digit2==b){
+cout<<"neon";}
+else{
+cout<<"nope";}
+
+
+
+
+
+
+return 0;}
