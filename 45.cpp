67cc7724912459dd8656a9185778c2667c73ca89
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-29 00:55:14 +0600

    Add files via upload

---
+++ 45.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int rows,i,j,k;
+cout<<"give rows ";
+cin>>rows;
+for(i=1;i<=rows;i++){
+for(j=1;j<=rows-i;j++){
+cout<<" ";}
+for(k=1;k<=2*i-1;k++){
+if(i==1||i==rows||k==1||k==2*i-1){
+cout<<"*";}
+else{
+cout<<" ";}}
+cout<<endl;}
+
+return 0;}
