cd530c249160bd8402c3bdef41d8f0f113c8ec60
mirza-meheraz-shafait-plabon <mirzaplabon34@gmail.com>
2025-08-28 22:04:26 +0600

    Add files via upload

---
+++ 43.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+char n;
+cout<<"give a character ";
+cin>>n;
+for(char i='a';i<=n;i++){
+for(char j='a';j<=n;j++){
+cout<<i<<" ";}
+cout<<endl;}
+
+
+
+
+return 0;}
